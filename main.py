92ed0ec0af89309dfc615c0676212476babfc70d
hunter <watermelonmcmuffintv@gmail.com>
2023-01-21 10:31:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+REDIRECT_IP = "104.238.130.180"
+
+
+def main():
+    """
+    Python script for setting up BIND9 with Bedrock Connect, since I couldn't get their shell script to
+    work properly.
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    main()
